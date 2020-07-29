45ebc7a5229856a5dc21d33cf2c3774e01ba4bd7
unknown <vaibhav.brkn@gmail.com>
2020-07-29 18:02:18 +0530

    Assignment1

---
+++ assignment1/second.py
@@ -0,0 +1,7 @@
+"""Write a Python program to accept the user's first and last name and then getting them
+printed in the the reverse order with a space between first name and last name."""
+
+first = input('Enter First Name: ')
+last = input('Enter Last Name: ')
+
+print(last , first)
---
+++ assignment1/third.py
@@ -0,0 +1,8 @@
+"""Write a Python program to find the volume of a sphere with diameter 12 cm."""
+
+diameter = 12
+radius = diameter / 2
+pie = 3.14
+
+volume = (4/3) * pie * (radius**3)
+print('Volume of sphere is : {}'.format(volume))
